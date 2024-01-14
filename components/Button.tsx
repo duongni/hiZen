a017c8deb0660d4fe15caf3b58fc85997727c201
duongni <133175915+duongni@users.noreply.github.com>
2024-01-14 17:15:19 -0500

    work on GetApp section

--- components/Button.tsx
+++ components/Button.tsx
@@ -6,22 +6,24 @@ type ButtonProps = {
   //icon is optional
   icon?: string;
   variant: string;
+  full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border ${variant}`}
+      className={`flexCenter gap-3 rounded-full border ${variant} ${
+        full && "w-full"
+      }`}
       type={type}
     >
       {" "}
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <label className="bold-16 whitespace-normal">{title}</label>
+      <label className="bold-16 whitespace-normal cursor-pointer">
+        {title}
+      </label>
     </button>
   );
 };
 
-const FeatureItem = () => {
-  return <div>{title}</div>;
-};
 export default Button;
